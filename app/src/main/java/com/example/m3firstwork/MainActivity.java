a110952314fdca6d71945250ce94268d83c5e28f
Sergan2B <IASer01gan2B>
2021-12-09 23:10:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/m3firstwork/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.m3firstwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView image = findViewById(R.id.imageView);
+        Glide.with(this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(image);
+    }
+}
